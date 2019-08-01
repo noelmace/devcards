1c356940fec3dc1b2cf09351c7bd401fb7f7d014
Noel Mace <contact@noelmace.com>
2019-08-01 15:08:12 +0200

    fix: html & css template tags
    Forgot the template keys... Duh!

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -1,23 +1,25 @@
 /**
- * Template tag without any effect
- * Only permits dev tools (like Prettier) to identify template strings as css
+ * Identity template tag
+ * permits dev tools (like Prettier) to identify template strings as css
+ * @deprecated Only for teaching purpose! DON'T USE IN PRODUCTION!
  * @example
  *  css`
  *    .title { color: red }
  *  `
  */
-export function css(t) {
-  return t
+export function css(strings, ...keys) {
+  return strings.reduce((rslt, str, i) => `${rslt}${keys[i]}`)
 }
 
 /**
- * Template tag without any effect
- * Only permits dev tools (like Prettier) to identify template strings as html
+ * Identity template tag
+ * permits dev tools (like Prettier) to identify template strings as html
+ * @deprecated Only for teaching purpose! DON'T USE IN PRODUCTION!
  * @example
  *  html`
  *    <div>Hello World!</div>
  *  `
  */
-export function html(t) {
-  return t
+export function html(strings, ...keys) {
+  return strings.reduce((rslt, str, i) => `${rslt}${keys[i]}`)
 }

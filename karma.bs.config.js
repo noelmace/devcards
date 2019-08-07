dc5f32e9c629eb3086fc613e834ddb13df15d27d
Noel Mace <contact@noelmace.com>
2019-08-07 12:09:09 +0200

    build: add testing using @open-wc
    Used `npm init @open-wc` > testing in order to add testing capabilities
    w/ Karma, Mocha & Chai (https://open-wc.org/testing/)

---
+++ karma.bs.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// See https://open-wc.org/testing/testing-karma-bs.html
+const merge = require('webpack-merge');
+const bsSettings = require('@open-wc/testing-karma-bs/bs-settings.js');
+const createBaseConfig = require('./karma.conf.js');
+
+module.exports = config => {
+  config.set(
+    merge(bsSettings(config), createBaseConfig(config), {
+      browserStack: {
+        // project: 'BS worker project name',
+      },
+    }),
+  );
+
+  return config;
+};

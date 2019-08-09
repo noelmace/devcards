a5027fee8d6b3dc71882041e3a56ea0e0bb0c120
Noel Mace <contact@noelmace.com>
2019-08-09 15:40:56 +0200

    test: flashcard flipping

--- test/open-wc/flashcard.test.js
+++ test/open-wc/flashcard.test.js
@@ -6,35 +6,112 @@ chai.use(chaiDom);
 
 import '../../src/components/flashcard/flashcard.js';
 
-describe('no content provided', () => {
-  let el;
+describe('dc-flashcard component', () => {
+  describe('Given: no initial slot / light DOM', () => {
+    let el;
 
-  beforeEach('Given: no initial slot / light DOM', async () => {
-    // Given: no initial slot / light DOM
-    el = await fixture('<dc-flashcard></dc-flashcard>');
+    beforeEach(async () => {
+      el = await fixture('<dc-flashcard></dc-flashcard>');
+    });
+
+    it('show an error message in "question"', async () => {
+      // question slot fallback content
+      const questionContent = el.shadowRoot.querySelectorAll('slot[name=question] > *');
+
+      // Then: no new question slot in light DOM
+      expect(el).lightDom.to.be.empty;
+      //    And: default question is an error
+      expect(questionContent[0]).to.have.class('error');
+      //    And: there isn't anything else in the question slot
+      expect(questionContent).to.have.length(1);
+    });
+
+    it('show an error message in "answer"', async () => {
+      // answer slot fallback content
+      const answerContent = el.shadowRoot.querySelectorAll('slot[name=answer] > *');
+
+      // Then: no new answer slot in light DOM
+      expect(el).lightDom.to.be.empty;
+      //    And: default answer is an error
+      expect(answerContent[0]).to.have.class('error');
+      //    And: there isn't anything else in the answer slot
+      expect(answerContent).to.have.length(1);
+    });
   });
 
-  it('show an error message in "question"', async () => {
-    // question slot fallback content
-    const questionContent = el.shadowRoot.querySelectorAll('slot[name=question] > *');
+  describe('Given: no initial attribute', () => {
+    let el;
 
-    // Then: no new question slot in light DOM
-    expect(el).lightDom.to.be.empty;
-    //    And: default question is an error
-    expect(questionContent[0]).to.have.class('error');
-    //    And: there isn't anything else in the question slot
-    expect(questionContent).to.have.length(1);
-  });
+    beforeEach(async () => {
+      el = await fixture(html`
+        <dc-flashcard>
+          <p slot="question">
+            A question?
+          </p>
+          <p slot="answer">
+            An answer!
+          </p>
+        </dc-flashcard>
+      `);
+    });
 
-  it('show an error message in "answer"', async () => {
-    // answer slot fallback content
-    const answerContent = el.shadowRoot.querySelectorAll('slot[name=answer] > *');
+    const testNoRotation = () => {
+      it('the flipped property is false', () => {
+        expect(el).to.have.property('flipped').that.is.false;
+      })
 
-    // Then: no new answer slot in light DOM
-    expect(el).lightDom.to.be.empty;
-    //    And: default answer is an error
-    expect(answerContent[0]).to.have.class('error');
-    //    And: there isn't anything else in the answer slot
-    expect(answerContent).to.have.length(1);
+      it(`flashcard isn't rotated`, async () => {
+        const flashcardStyle = getComputedStyle(el.shadowRoot.querySelector('.flashcard'));
+        expect(flashcardStyle)
+          .to.have.property('transform')
+          .that.equal('none');
+      });
+    };
+
+    testNoRotation();
+
+    const testRotation = () => {
+      it('the flipped property is true', () => {
+        expect(el).to.have.property('flipped').that.is.true;
+      })
+
+      it('flashcard has been rotated by 180deg', async () => {
+        const flashcardStyle = getComputedStyle(el.shadowRoot.querySelector('.flashcard'));
+        expect(flashcardStyle).to.have.property('transform').that.is.equal('matrix(1, 0, 0, 1, 0, 0)');
+      });
+    };
+
+    describe('When: the flipped attribute is added', () => {
+      beforeEach(() => {
+        el.setAttribute('flipped', '');
+      })
+
+      testRotation()
+    });
+
+    describe('When: the flip method is called', () => {
+      beforeEach(() => {
+        el.flip();
+      })
+
+      testRotation()
+    });
+
+    describe('When: the flip method is called twice', () => {
+      beforeEach(() => {
+        el.flip();
+        el.flip();
+      })
+
+      testNoRotation()
+    });
+
+    describe('When: click on the card', () => {
+      beforeEach(() => {
+        el.click();
+      })
+
+      testRotation()
+    });
   });
 });

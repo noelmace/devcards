e32e571af657b0e091fa0d210684baafe57cd301
Noel Mace <contact@noelmace.com>
2019-08-12 21:57:17 +0200

    test: card-stack - event spy w/ sinon & jasmine
    Demo of how to use Jasmine & Simon to test WC custom events.

--- test/jasmine/components/card-stack.test.js
+++ test/jasmine/components/card-stack.test.js
@@ -54,7 +54,7 @@ describe('dc-stack component', () => {
     it('render the collection using dc-flashcard', () => {
       const cardsEl = el.shadowRoot.querySelectorAll('dc-flashcard');
       expect(cardsEl.length).toBe(mock.length);
-      const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim())
+      const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim());
       const mockQuestions = mock.map(card => card.question);
       expect(renderedQuestions).toEqual(mockQuestions);
     });
@@ -89,8 +89,93 @@ describe('dc-stack component', () => {
 
         it('the last flashcard is removed from the DOM', () => {
           expect(el.shadowRoot.querySelectorAll('dc-flashcard').length).toBe(mock.length - 1);
-          const lastFlashcardEl = el.shadowRoot.querySelector('.card-wrapper:last-child [slot="question"]').innerHTML.trim();
+          const lastFlashcardEl = el.shadowRoot
+            .querySelector('.card-wrapper:last-child [slot="question"]')
+            .innerHTML.trim();
           expect(lastFlashcardEl).toEqual(mock[mock.length - 2].question);
+        });
+
+        it(`the collection hasn't changed`, function() {
+          expect(el.collection).toEqual(mock);
+        });
+
+        describe(`When: there isn't any card left in the current deck`, () => {
+          let emptyStackEventSpy;
+          beforeEach(() => {
+            emptyStackEventSpy = jasmine.createSpy('empty-stack event');
+            el.addEventListener('empty-stack', emptyStackEventSpy);
+
+            el.pop();
+            el.pop();
+          });
+
+          it('a message is rendered', () => {
+            expect(el.shadowRoot.querySelector('.empty-box > p').innerText.trim())
+              .toEqual('You finished this session! Click the reload button to review this collection again.');
+          });
+
+          it('a reload button is rendered', () => {
+            expect(el.shadowRoot.querySelector('i.reload'))
+              .not.toBeNull();
+          });
+
+          it('one "empty-stack" event is fired', () => {
+            expect(emptyStackEventSpy).toHaveBeenCalledTimes(1);
+          })
+
+          describe('When: click on the reload button', () => {
+            let reloadFromCollectionEventSpy;
+
+            beforeEach(() => {
+              reloadFromCollectionEventSpy = jasmine.createSpy()
+              el.addEventListener('reload-collection', reloadFromCollectionEventSpy);
+              el.shadowRoot.querySelector('i.reload').click()
+            })
+
+            it('one "empty-stack" event is fired ', () => {
+              expect(reloadFromCollectionEventSpy).toHaveBeenCalledTimes(1)
+            })
+
+            testRenderCollection()
+          })
+
+        });
+      });
+
+      describe('When: moveBack() is called', () => {
+        beforeEach(() => {
+          el.moveBack();
+        });
+
+        it('the last card is moved to the bottom of the deck', () => {
+          const movedMock = [mock[mock.length - 1], ...mock.slice(0, -1)];
+          expect(el.currentDeck).toEqual(movedMock);
+
+          const lastFlashcardEl = el.shadowRoot
+            .querySelector('.card-wrapper:last-child [slot="question"]')
+            .innerHTML.trim();
+          expect(lastFlashcardEl).toEqual(movedMock[movedMock.length - 1].question);
+
+          const firstFlashcardEl = el.shadowRoot
+            .querySelector('.card-wrapper:first-child [slot="question"]')
+            .innerHTML.trim();
+          expect(firstFlashcardEl).toEqual(movedMock[0].question);
+        });
+      });
+
+      describe('When: the first card is flipped and then moveBack is called', () => {
+        let flippedCard;
+        beforeEach(() => {
+          flippedCard = el.shadowRoot.querySelector('.card-wrapper:last-child dc-flashcard');
+          flippedCard.setAttribute('flipped', '');
+          el.moveBack();
+        });
+
+        it(`the card that have been moved isn't flipped`, () => {
+          expect(flippedCard.hasAttribute('flipped')).toBe(false);
+          expect(
+            el.shadowRoot.querySelector('.card-wrapper:first-child [slot="question"]').hasAttribute('flipped')
+          ).toBe(false);
         });
       });
 
--- test/open-wc/components/card-stack.test.js
+++ test/open-wc/components/card-stack.test.js
@@ -1,4 +1,5 @@
 import { fixture, expect, chai, nextFrame } from '@open-wc/testing';
+import sinon from 'sinon';
 
 import { chaiDom } from '../../utils/chai-dom/chai-dom.js';
 
@@ -40,7 +41,7 @@ function testNoCollectionMsg() {
   it('a "no valid collection" message is shown', function() {
     expect(this.el).shadowDom.to.equal('<div class="cards-stack"><p>No valid collection</p></div>');
   });
-};
+}
 
 /**
  * Use the this context for mocks
@@ -53,16 +54,14 @@ function testRenderCollection() {
   it('render the collection using dc-flashcard', function() {
     const cardsEl = this.el.shadowRoot.querySelectorAll('dc-flashcard');
     expect(cardsEl).to.have.lengthOf(this.mock.length);
-    const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim())
+    const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim());
     const mockQuestions = this.mock.map(card => card.question);
     expect(renderedQuestions).to.deep.equal(mockQuestions);
   });
-};
+}
 
 describe('dc-stack component', function() {
-
-  describe('Given: no argument nor content', function () {
-
+  describe('Given: no argument nor content', function() {
     beforeEach(async function() {
       this.el = await fixture('<dc-stack></dc-stack');
     });
@@ -70,7 +69,6 @@ describe('dc-stack component', function() {
     testNoCollectionMsg();
 
     describe('When: an initial collection is given', function() {
-
       beforeEach(function() {
         this.mock = collectionMock;
         this.el.collection = this.mock;
@@ -79,21 +77,103 @@ describe('dc-stack component', function() {
       testRenderCollection();
 
       describe('When: pop() is called', function() {
-        beforeEach(async function() {
+        beforeEach(function() {
           this.popped = this.el.pop();
-          await nextFrame();
-        })
-
-        it('the last card is removed from the current deck', function(){
-          expect(this.el).to.have.property('currentDeck').that.is.an('array').that.deep.equal(this.mock.slice(0, -1));
         });
 
-        it('the last flashcard is removed from the DOM', function(){
+        it('the last card is removed from the current deck', function() {
+          expect(this.el)
+            .to.have.property('currentDeck')
+            .that.is.an('array')
+            .that.deep.equal(this.mock.slice(0, -1));
+        });
+
+        it('the last flashcard is removed from the DOM', function() {
           expect(this.el.shadowRoot.querySelectorAll('dc-flashcard').length).to.equal(this.mock.length - 1);
-          const lastFlashcardEl = this.el.shadowRoot.querySelector('.card-wrapper:last-child [slot="question"]').innerHTML.trim();
+          const lastFlashcardEl = this.el.shadowRoot
+            .querySelector('.card-wrapper:last-child [slot="question"]')
+            .innerHTML.trim();
           expect(lastFlashcardEl).to.equal(this.mock[this.mock.length - 2].question);
         });
+
+        it(`the collection hasn't changed`, function() {
+          expect(this.el.collection).to.deep.equal(this.mock);
+        });
+
+        describe(`When: there isn't any card left in the current deck`, () => {
+          beforeEach(function() {
+            this.emptyStackEventSpy = sinon.spy();
+            this.el.addEventListener('empty-stack', this.emptyStackEventSpy);
+
+            this.el.pop();
+            this.el.pop();
+          });
+
+          it('a message is rendered', function() {
+            expect(this.el.shadowRoot)
+              .to.have.descendant('.empty-box > p')
+              .that.have.text('You finished this session! Click the reload button to review this collection again.');
+          });
+
+          it('a reload button is rendered', function() {
+            expect(this.el.shadowRoot)
+              .to.have.descendant('i.reload');
+          });
+
+          it('one "empty-stack" event is fired', function() {
+            expect(this.emptyStackEventSpy.callCount).to.equal(1)
+          })
+
+          describe('When: click on the reload button', function() {
+            beforeEach(function() {
+              this.reloadFromCollectionEventSpy = sinon.spy()
+              this.el.addEventListener('reload-collection', this.reloadFromCollectionEventSpy);
+              this.el.shadowRoot.querySelector('i.reload').click()
+            })
+
+            it('one "empty-stack" event is fired ', function() {
+              expect(this.reloadFromCollectionEventSpy.callCount).to.equal(1)
+            })
+
+            testRenderCollection()
+          })
+
+        });
       });
+
+      describe('When: moveBack() is called', function() {
+        beforeEach(function() {
+          this.el.moveBack();
+        });
+
+        it('the last card is moved to the bottom of the deck', function() {
+          const movedMock = [this.mock[this.mock.length - 1], ...this.mock.slice(0, -1)]
+          expect(this.el).to.have.property('currentDeck').that.deep.equals(movedMock)
+
+          const lastFlashcardEl = this.el.shadowRoot
+            .querySelector('.card-wrapper:last-child [slot="question"]')
+            .innerHTML.trim();
+          expect(lastFlashcardEl).to.equal(movedMock[movedMock.length - 1].question);
+
+          const firstFlashcardEl = this.el.shadowRoot
+            .querySelector('.card-wrapper:first-child [slot="question"]')
+            .innerHTML.trim();
+          expect(firstFlashcardEl).to.equal(movedMock[0].question);
+        })
+      });
+
+      describe('When: the first card is flipped and then moveBack is called', function() {
+        beforeEach(function() {
+          this.flippedCard = this.el.shadowRoot.querySelector('.card-wrapper:last-child dc-flashcard');
+          this.flippedCard.setAttribute('flipped', '');
+          this.el.moveBack();
+        })
+
+        it(`the card that have been moved isn't flipped`, function() {
+          expect(this.flippedCard).to.not.have.attribute('flipped');
+          expect(this.el.shadowRoot).to.have.descendant('.card-wrapper:first-child [slot="question"]').that.not.have.attribute('flipped');
+        });
+      })
 
       describe('When: an new collection is given', function() {
         beforeEach(function() {

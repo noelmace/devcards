cd32cf0088a5833501cf02cd6bf260cc86673f7a
Noel Mace <contact@noelmace.com>
2019-08-12 11:01:08 +0200

    REPRO - context issue with mocha Don't merge!
    Passing context to shared behavior using arguments fails because beforeEach is called after the shared behavior.
    See https://github.com/open-wc/open-wc/issues/715#issuecomment-520149234

--- test/open-wc/components/card-stack.test.js
+++ test/open-wc/components/card-stack.test.js
@@ -36,9 +36,9 @@ const collectionMock2 = [
   }
 ];
 
-function testNoCollectionMsg() {
-  it('a "no valid collection" message is shown', function() {
-    expect(this.el).shadowDom.to.equal('<div class="cards-stack"><p>No valid collection</p></div>');
+function testNoCollectionMsg(el) {
+  it('a "no valid collection" message is shown', () => {
+    expect(el).shadowDom.to.equal('<div class="cards-stack"><p>No valid collection</p></div>');
   });
 };
 
@@ -49,59 +49,61 @@ function testNoCollectionMsg() {
  * TODO: see if we can use context in a more modern way
  * See https://github.com/mochajs/mocha/issues/1856
  */
-function testRenderCollection() {
-  it('render the collection using dc-flashcard', function() {
-    const cardsEl = this.el.shadowRoot.querySelectorAll('dc-flashcard');
-    expect(cardsEl).to.have.lengthOf(this.mock.length);
-    for (let i = 0; i < this.mock.length; i++) {
+function testRenderCollection(el, mock) {
+  it('render the collection using dc-flashcard', () => {
+    const cardsEl = el.shadowRoot.querySelectorAll('dc-flashcard');
+    expect(cardsEl).to.have.lengthOf(mock.length);
+    for (let i = 0; i < mock.length; i++) {
       const questionSlotContent = cardsEl[i].querySelector('[slot="question"]').innerHTML.trim();
-      expect(questionSlotContent, `dc-card ${i}`).to.equals(this.mock[i].question);
+      expect(questionSlotContent, `dc-card ${i}`).to.equals(mock[i].question);
     }
   });
 };
 
-describe('dc-stack component', () => {
+describe.only('dc-stack component', () => {
 
-  describe('Given: no argument nor content', function () {
-    beforeEach(async function() {
-      this.el = await fixture('<dc-stack></dc-stack');
+  let mock, el;
+
+  describe('Given: no argument nor content', () => {
+    beforeEach(async () => {
+      el = await fixture('<dc-stack></dc-stack');
     });
 
-    testNoCollectionMsg();
+    testNoCollectionMsg(el);
 
     describe('When: an initial collection is given', () => {
 
-      beforeEach(function() {
-        this.mock = collectionMock;
-        this.el.collection = this.mock;
+      beforeEach(() => {
+        mock = collectionMock;
+        el.collection = mock;
       });
 
-      testRenderCollection();
+      testRenderCollection(el, mock);
 
       describe('When: an new collection is given', () => {
-        beforeEach(function() {
-          this.mock = collectionMock2;
-          this.el.collection = this.mock;
+        beforeEach(() => {
+          mock = collectionMock2;
+          el.collection = mock;
         });
 
-        testRenderCollection();
+        testRenderCollection(el, mock);
       });
     });
 
     describe('When: an invalid collection is given', () => {
-      beforeEach(function() {
-        this.el.collection = 'bad';
+      beforeEach(() => {
+        el.collection = 'bad';
       });
 
-      testNoCollectionMsg();
+      testNoCollectionMsg(el);
     });
 
     describe('When: an empty collection is given', () => {
       beforeEach(function() {
-        this.el.collection = [];
+        el.collection = [];
       });
 
-      testNoCollectionMsg();
+      testNoCollectionMsg(el);
     });
   });
 });

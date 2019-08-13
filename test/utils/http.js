f73d97d8c7359862a3b0b286dc394641676b313d
Noel Mace <contact@noelmace.com>
2019-08-13 20:08:18 +0200

    test: dc-repetition
    Coverage is finally acceptable when using open-wc!
    At last, I can start refactoring!
    But first, I need to add an example of stub for web component definitions...

--- test/utils/http.js
+++ test/utils/http.js
@@ -3,3 +3,9 @@ export const mockResponse200 = body => () =>
     status: 200,
     headers: { 'Content-type': 'application/json' }
   });
+
+export const mockResponse404 = () =>
+  new Response('nope', {
+    status: 404,
+    headers: { 'Content-type': 'text/html' }
+  });

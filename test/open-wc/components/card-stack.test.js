42094dfe726fc14753abca133f9c07e1b2fe684e
Noel Mace <contact@noelmace.com>
2019-08-21 20:36:12 +0200

    refactor: follow my own shared behavior best practices

--- test/open-wc/components/card-stack.test.js
+++ test/open-wc/components/card-stack.test.js
@@ -5,165 +5,169 @@ import '../../../src/components/card-stack/card-stack.js';
 
 import { collectionMock, collectionMock2 } from '../../mocks/collection.js';
 
-function testNoCollectionMsg() {
-  it('a "no valid collection" message is shown', function() {
-    expect(this.el).shadowDom.to.equal('<div class="cards-stack"><p>No valid collection</p></div>');
-  });
-}
+describe('dc-stack component', () => {
+  let el, mock;
 
-/**
- * Use the this context for mocks
- * therefore, it needs that all beforeEach use anonymous functions instead of arrow functions
- * See https://github.com/mochajs/mocha/wiki/Shared-Behaviours
- * TODO: see if we can use context in a more modern way
- * See https://github.com/mochajs/mocha/issues/1856
- */
-function testRenderCollection() {
-  it('render the collection using dc-flashcard', function() {
-    const cardsEl = this.el.shadowRoot.querySelectorAll('dc-flashcard');
-    expect(cardsEl).to.have.lengthOf(this.mock.length);
-    const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim());
-    const mockQuestions = this.mock.map(card => card.question);
-    expect(renderedQuestions).to.deep.equal(mockQuestions);
-  });
-}
+  const testNoCollectionMsg = () => {
+    it('a "no valid collection" message is shown', () => {
+      expect(el).shadowDom.to.equal('<div class="cards-stack"><p>No valid collection</p></div>');
+    });
+  };
 
-describe('dc-stack component', function() {
-  describe('Given: no argument nor content', function() {
-    beforeEach(async function() {
-      this.el = await fixture('<dc-stack></dc-stack>');
+  const testRenderCollection = () => {
+    it('render the collection using dc-flashcard', () => {
+      const cardsEl = el.shadowRoot.querySelectorAll('dc-flashcard');
+      expect(cardsEl).to.have.lengthOf(mock.length);
+      const renderedQuestions = [...cardsEl].map(elmt => elmt.querySelector('[slot="question"]').innerHTML.trim());
+      const mockQuestions = mock.map(card => card.question);
+      expect(renderedQuestions).to.deep.equal(mockQuestions);
+    });
+  };
+
+  describe('Given: no argument nor content', () => {
+    beforeEach(async () => {
+      el = await fixture('<dc-stack></dc-stack>');
     });
 
     testNoCollectionMsg();
 
-    describe('When: an initial collection is given', function() {
-      beforeEach(function() {
-        this.mock = collectionMock;
-        this.el.collection = this.mock;
+    describe('When: an initial collection is given', () => {
+      beforeEach(() => {
+        mock = collectionMock;
+        el.collection = mock;
       });
 
       testRenderCollection();
 
-      describe('When: pop() is called', function() {
-        beforeEach(function() {
-          this.popped = this.el.pop();
+      describe('When: pop() is called', () => {
+        let popped;
+
+        beforeEach(() => {
+          popped = el.pop();
         });
 
-        it('the last card is removed from the current deck', function() {
-          expect(this.el)
+        it('the last card is removed from the current deck', () => {
+          expect(el)
             .to.have.property('currentDeck')
             .that.is.an('array')
-            .that.deep.equal(this.mock.slice(0, -1));
+            .that.deep.equal(mock.slice(0, -1));
         });
 
-        it('the last flashcard is removed from the DOM', function() {
-          expect(this.el.shadowRoot.querySelectorAll('dc-flashcard').length).to.equal(this.mock.length - 1);
-          const lastFlashcardEl = this.el.shadowRoot
+        it('the last flashcard is removed from the DOM', () => {
+          expect(el.shadowRoot.querySelectorAll('dc-flashcard').length).to.equal(mock.length - 1);
+          const lastFlashcardEl = el.shadowRoot
             .querySelector('.card-wrapper:last-child [slot="question"]')
             .innerHTML.trim();
-          expect(lastFlashcardEl).to.equal(this.mock[this.mock.length - 2].question);
+          expect(lastFlashcardEl).to.equal(mock[mock.length - 2].question);
         });
 
-        it(`the collection hasn't changed`, function() {
-          expect(this.el.collection).to.deep.equal(this.mock);
+        it(`the collection hasn't changed`, () => {
+          expect(el.collection).to.deep.equal(mock);
         });
 
         describe(`When: there isn't any card left in the current deck`, () => {
-          beforeEach(function() {
-            this.emptyStackEventSpy = sinon.spy();
-            this.el.addEventListener('empty-stack', this.emptyStackEventSpy);
+          let emptyStackEventSpy;
 
-            this.el.pop();
-            this.el.pop();
+          beforeEach(() => {
+            emptyStackEventSpy = sinon.spy();
+            el.addEventListener('empty-stack', emptyStackEventSpy);
+
+            el.pop();
+            el.pop();
           });
 
-          it('a message is rendered', function() {
-            expect(this.el.shadowRoot)
+          it('a message is rendered', () => {
+            expect(el.shadowRoot)
               .to.have.descendant('.empty-box > p')
               .that.have.text('You finished this session! Click the reload button to review this collection again.');
           });
 
-          it('a reload button is rendered', function() {
-            expect(this.el.shadowRoot)
-              .to.have.descendant('i.reload');
+          it('a reload button is rendered', () => {
+            expect(el.shadowRoot).to.have.descendant('i.reload');
           });
 
-          it('one "empty-stack" event is fired', function() {
-            expect(this.emptyStackEventSpy.callCount).to.equal(1)
-          })
+          it('one "empty-stack" event is fired', () => {
+            expect(emptyStackEventSpy.callCount).to.equal(1);
+          });
 
-          describe('When: click on the reload button', function() {
-            beforeEach(function() {
-              this.reloadFromCollectionEventSpy = sinon.spy()
-              this.el.addEventListener('reload-collection', this.reloadFromCollectionEventSpy);
-              this.el.shadowRoot.querySelector('i.reload').click()
-            })
+          describe('When: click on the reload button', () => {
+            let reloadFromCollectionEventSpy;
 
-            it('one "empty-stack" event is fired ', function() {
-              expect(this.reloadFromCollectionEventSpy.callCount).to.equal(1)
-            })
+            beforeEach(() => {
+              reloadFromCollectionEventSpy = sinon.spy();
+              el.addEventListener('reload-collection', reloadFromCollectionEventSpy);
+              el.shadowRoot.querySelector('i.reload').click();
+            });
 
-            testRenderCollection()
-          })
+            it('one "empty-stack" event is fired ', () => {
+              expect(reloadFromCollectionEventSpy.callCount).to.equal(1);
+            });
 
+            testRenderCollection();
+          });
         });
       });
 
-      describe('When: moveBack() is called', function() {
-        beforeEach(function() {
-          this.el.moveBack();
+      describe('When: moveBack() is called', () => {
+        beforeEach(() => {
+          el.moveBack();
         });
 
-        it('the last card is moved to the bottom of the deck', function() {
-          const movedMock = [this.mock[this.mock.length - 1], ...this.mock.slice(0, -1)]
-          expect(this.el).to.have.property('currentDeck').that.deep.equals(movedMock)
+        it('the last card is moved to the bottom of the deck', () => {
+          const movedMock = [mock[mock.length - 1], ...mock.slice(0, -1)];
+          expect(el)
+            .to.have.property('currentDeck')
+            .that.deep.equals(movedMock);
 
-          const lastFlashcardEl = this.el.shadowRoot
+          const lastFlashcardEl = el.shadowRoot
             .querySelector('.card-wrapper:last-child [slot="question"]')
             .innerHTML.trim();
           expect(lastFlashcardEl).to.equal(movedMock[movedMock.length - 1].question);
 
-          const firstFlashcardEl = this.el.shadowRoot
+          const firstFlashcardEl = el.shadowRoot
             .querySelector('.card-wrapper:first-child [slot="question"]')
             .innerHTML.trim();
           expect(firstFlashcardEl).to.equal(movedMock[0].question);
-        })
+        });
       });
 
-      describe('When: the first card is flipped and then moveBack is called', function() {
-        beforeEach(function() {
-          this.flippedCard = this.el.shadowRoot.querySelector('.card-wrapper:last-child dc-flashcard');
-          this.flippedCard.setAttribute('flipped', '');
-          this.el.moveBack();
-        })
-
-        it(`the card that have been moved isn't flipped`, function() {
-          expect(this.flippedCard).to.not.have.attribute('flipped');
-          expect(this.el.shadowRoot).to.have.descendant('.card-wrapper:first-child [slot="question"]').that.not.have.attribute('flipped');
+      describe('When: the first card is flipped and then moveBack is called', () => {
+        let flippedCard;
+        beforeEach(() => {
+          flippedCard = el.shadowRoot.querySelector('.card-wrapper:last-child dc-flashcard');
+          flippedCard.setAttribute('flipped', '');
+          el.moveBack();
         });
-      })
 
-      describe('When: an new collection is given', function() {
-        beforeEach(function() {
-          this.mock = collectionMock2;
-          this.el.collection = this.mock;
+        it(`the card that have been moved isn't flipped`, () => {
+          expect(flippedCard).to.not.have.attribute('flipped');
+          expect(el.shadowRoot)
+            .to.have.descendant('.card-wrapper:first-child [slot="question"]')
+            .that.not.have.attribute('flipped');
+        });
+      });
+
+      describe('When: an new collection is given', () => {
+        beforeEach(() => {
+          mock = collectionMock2;
+          el.collection = mock;
         });
 
         testRenderCollection();
       });
     });
 
-    describe('When: an invalid collection is given', function() {
-      beforeEach(function() {
-        this.el.collection = 'bad';
+    describe('When: an invalid collection is given', () => {
+      beforeEach(() => {
+        el.collection = 'bad';
       });
 
       testNoCollectionMsg();
     });
 
-    describe('When: an empty collection is given', function() {
-      beforeEach(function() {
-        this.el.collection = [];
+    describe('When: an empty collection is given', () => {
+      beforeEach(() => {
+        el.collection = [];
       });
 
       testNoCollectionMsg();
--- test/open-wc/components/repetition.test.js
+++ test/open-wc/components/repetition.test.js
@@ -6,110 +6,118 @@ import { mockResponse200, mockResponse404 } from '../../utils/http.js';
 import { collectionMock } from '../../mocks/collection.js';
 import sinon from 'sinon';
 
-/**
- * @param {RegExp} urlPattern
- */
-function testFetch(urlPattern) {
-  it('data is fetched', function() {
-    expect(this.fetchStub).to.have.been.calledWithMatch({ url: sinon.match(urlPattern) });
-  });
-}
+describe('dc-repetition component', () => {
+  let fetchStub, mockCollectionName, el;
 
-function testCardsRendering(collection) {
-  it('cards are rendered into the first box', function() {
-    expect(this.el.shadowRoot)
-      .to.have.descendant('.stack-0')
-      .which.has.property('collection')
-      .that.deep.equals(collection);
-  });
-}
-
-describe('dc-repetition component', function() {
-
-  beforeEach(function() {
-    this.fetchStub = sinon.stub(window, 'fetch');
-    this.mockCollectionName = 'foo';
+  beforeEach(() => {
+    fetchStub = sinon.stub(window, 'fetch');
+    mockCollectionName = 'foo';
   });
 
-  describe('Given: no argument nor content', function() {
-    beforeEach(async function() {
-      this.el = await fixture('<dc-repetition></dc-repetition>');
+  /**
+   * @param {RegExp} urlPattern
+   */
+  const testFetch = urlPattern => {
+    it('data is fetched', () => {
+      expect(fetchStub).to.have.been.calledWithMatch({ url: sinon.match(urlPattern) });
+    });
+  };
+
+  const testCardsRendering = collection => {
+    it('cards are rendered into the first box', () => {
+      expect(el.shadowRoot)
+        .to.have.descendant('.stack-0')
+        .which.has.property('collection')
+        .that.deep.equals(collection);
+    });
+  };
+
+  describe('Given: no argument nor content', () => {
+    beforeEach(async () => {
+      el = await fixture('<dc-repetition></dc-repetition>');
     });
 
-    it(`asn't any collection by default`, function() {
-      expect(this.el.collection).to.be.null;
-      expect(this.el.shadowRoot)
+    it(`asn't any collection by default`, () => {
+      expect(el.collection).to.be.null;
+      expect(el.shadowRoot)
         .to.have.descendant('dc-stack')
         .that.has.property('collection').which.is.null;
     });
 
-    describe('When: a valid collection name is given', function() {
-      beforeEach(function() {
+    describe('When: a valid collection name is given', () => {
+      beforeEach(() => {
         // don't use stub.resolves or stub.returns here, because the same Response instance would be used every time
         // which could leads to a "Failed to execute 'json' on 'Response': body stream is locked" error
         // FYI, doing so, you could also use before instead of beforeEach, but only if you don't need to count calls or
         // verify an abscence of call
-        // but this doesn't make a real difference with performance, so, favor beforeEach by default when mocking fetch
-        this.fetchStub.callsFake(mockResponse200(collectionMock));
+        // but  doesn't make a real difference with performance, so, favor beforeEach by default when mocking fetch
+        fetchStub.callsFake(mockResponse200(collectionMock));
       });
 
-      beforeEach(async function() {
-        this.el.setAttribute('collection', 'foo');
-        await elementUpdated(this.el);
+      beforeEach(async () => {
+        el.setAttribute('collection', 'foo');
+        await elementUpdated(el);
       });
 
       testFetch(/\/data\/foo\.json$/);
 
       testCardsRendering(collectionMock);
 
-      it(`the collection and the collection in dc-stack are'nt the same instance`, function() {
-        expect(this.el.shadowRoot).to.have.descendant('.stack-0').that.have.property('collection').that.not.equal(this.el.boxes[0]);
+      it(`the collection and the collection in dc-stack are'nt the same instance`, () => {
+        expect(el.shadowRoot)
+          .to.have.descendant('.stack-0')
+          .that.have.property('collection')
+          .that.not.equal(el.boxes[0]);
       });
 
-      describe('When: a button is clicked', function() {
-        beforeEach(function() {
-          this.firstStackEl = this.el.shadowRoot.querySelector('.stack-0');
-          sinon.spy(this.firstStackEl, 'pop');
-          sinon.spy(this.firstStackEl, 'moveBack');
+      describe('When: a button is clicked', () => {
+        let firstStackEl;
+
+        beforeEach(() => {
+          firstStackEl = el.shadowRoot.querySelector('.stack-0');
+          sinon.spy(firstStackEl, 'pop');
+          sinon.spy(firstStackEl, 'moveBack');
         });
 
-        describe('"ok" button', function() {
-          beforeEach(function() {
-            this.el.shadowRoot.querySelector('.action-ok').click();
+        describe('"ok" button', () => {
+          beforeEach(() => {
+            el.shadowRoot.querySelector('.action-ok').click();
           });
 
-          it('DeckStack.pop() has been called on the first stack', function() {
-            expect(this.firstStackEl.pop).to.have.been.calledOnce;
+          it('DeckStack.pop() has been called on the first stack', () => {
+            expect(firstStackEl.pop).to.have.been.calledOnce;
           });
 
-          it('first card is moved to the next box', function() {
-            expect(this.el)
+          it('first card is moved to the next box', () => {
+            expect(el)
               .to.have.property('boxes')
               .that.deep.equals([collectionMock.slice(0, -1), [collectionMock[collectionMock.length - 1]], []]);
           });
         });
 
-        describe('"nok" button', function() {
-          beforeEach(function() {
-            this.el.shadowRoot.querySelector('.action-nok').click();
+        describe('"nok" button', () => {
+          beforeEach(() => {
+            el.shadowRoot.querySelector('.action-nok').click();
           });
 
-          it('DeckStack.mockBack() has been called on the first stack', function() {
-            expect(this.firstStackEl.moveBack).to.have.been.calledOnce;
+          it('DeckStack.mockBack() has been called on the first stack', () => {
+            expect(firstStackEl.moveBack).to.have.been.calledOnce;
           });
         });
       });
 
-      describe(`When: an 'empty-stack' event is dispatched by a child component`, function() {
-        beforeEach(async function() {
-          this.el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('empty-stack'));
-          await elementUpdated(this.el);
-        })
+      describe(`When: an 'empty-stack' event is dispatched by a child component`, () => {
+        beforeEach(async () => {
+          el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('empty-stack'));
+          await elementUpdated(el);
+        });
 
-        it(`action buttons shouldn't be displayed`, function() {
-          expect(this.el.shadowRoot).to.have.descendant('.container').that.has.class('empty');
-         const actionWrappers = this.el.shadowRoot.querySelectorAll('.action-wrapper');
-          [...actionWrappers].forEach((action) => {
+        it(`action buttons shouldn't be displayed`, () => {
+          expect(el.shadowRoot)
+            .to.have.descendant('.container')
+            .that.has.class('empty');
+          const actionWrappers = el.shadowRoot.querySelectorAll('.action-wrapper');
+          [...actionWrappers].forEach(action => {
             const actionDisplay = getComputedStyle(action).display;
             expect(actionDisplay).to.equal('none');
             //// chai-dom gives a falls negative here
@@ -119,19 +127,21 @@ describe('dc-repetition component', function() {
             // expect(action).not.to.be.displayed;
             //// equals ''
             // expect(action.style.display).to.equal('none');
-          })
-        })
+          });
+        });
 
-        describe(`When: the current stack is empty`, function() {
-          beforeEach(async function() {
-            this.el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('empty-stack'));
-            await elementUpdated(this.el);
-          })
+        describe(`When: the current stack is empty`, () => {
+          beforeEach(async () => {
+            el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('empty-stack'));
+            await elementUpdated(el);
+          });
 
-          it(`action buttons shouldn't be displayed`, function() {
-            expect(this.el.shadowRoot).to.have.descendant('.container').that.has.class('empty');
-           const actionWrappers = this.el.shadowRoot.querySelectorAll('.action-wrapper');
-            [...actionWrappers].forEach((action) => {
+          it(`action buttons shouldn't be displayed`, () => {
+            expect(el.shadowRoot)
+              .to.have.descendant('.container')
+              .that.has.class('empty');
+            const actionWrappers = el.shadowRoot.querySelectorAll('.action-wrapper');
+            [...actionWrappers].forEach(action => {
               const actionDisplay = getComputedStyle(action).display;
               expect(actionDisplay).to.equal('none');
               //// chai-dom gives a falls negative here
@@ -141,71 +151,73 @@ describe('dc-repetition component', function() {
               // expect(action).not.to.be.displayed;
               //// equals ''
               // expect(action.style.display).to.equal('none');
-            })
-          })
+            });
+          });
 
-          describe(`When: the current stack is reloaded`, function() {
-            beforeEach(async function() {
-              this.el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('reload-collection'));
-              await elementUpdated(this.el);
-            })
+          describe(`When: the current stack is reloaded`, () => {
+            beforeEach(async () => {
+              el.shadowRoot.querySelector('.stack-0').dispatchEvent(new CustomEvent('reload-collection'));
+              await elementUpdated(el);
+            });
 
-            it(`action buttons should be displayed`, function() {
-              expect(this.el.shadowRoot).to.have.descendant('.container').that.not.has.class('empty');
-             const actionWrappers = this.el.shadowRoot.querySelectorAll('.action-wrapper');
-              [...actionWrappers].forEach((action) => {
+            it(`action buttons should be displayed`, () => {
+              expect(el.shadowRoot)
+                .to.have.descendant('.container')
+                .that.not.has.class('empty');
+              const actionWrappers = el.shadowRoot.querySelectorAll('.action-wrapper');
+              [...actionWrappers].forEach(action => {
                 const actionDisplay = getComputedStyle(action).display;
                 expect(actionDisplay).not.to.equal('none');
-              })
-            })
-          })
-        })
-      })
+              });
+            });
+          });
+        });
+      });
     });
 
-    describe('When: an invalid collection name is given', function() {
-      beforeEach(function() {
-        this.fetchStub.callsFake(mockResponse404);
+    describe('When: an invalid collection name is given', () => {
+      beforeEach(() => {
+        fetchStub.callsFake(mockResponse404);
       });
 
-      beforeEach(async function() {
-        this.el.setAttribute('collection', this.mockCollectionName);
-        await elementUpdated(this.el);
+      beforeEach(async () => {
+        el.setAttribute('collection', mockCollectionName);
+        await elementUpdated(el);
       });
 
       testFetch(/\/data\/foo\.json$/);
 
-      it('an error message is shown', function() {
-        expect(this.el.shadowRoot).to.have.descendant('.errors-container').that.is.visible.and.displayed;
-        expect(this.el.shadowRoot)
+      it('an error message is shown', () => {
+        expect(el.shadowRoot).to.have.descendant('.errors-container').that.is.visible.and.displayed;
+        expect(el.shadowRoot)
           // see https://github.com/nathanboktae/chai-dom/issues/30#issuecomment-372759090
           // in order to understand why semantic-dom-diff & chai-dom are so different
           .to.have.descendant('.errors-container > p')
-          .which.has.text(`No flashcard could be found for the ${this.mockCollectionName} collection.`);
+          .which.has.text(`No flashcard could be found for the ${mockCollectionName} collection.`);
       });
     });
 
-    describe('Given: collection attribute is set to a falsy value', function() {
-      beforeEach(async function() {
-        this.el.setAttribute('collection', '');
-        await elementUpdated(this.el);
+    describe('Given: collection attribute is set to a falsy value', () => {
+      beforeEach(async () => {
+        el.setAttribute('collection', '');
+        await elementUpdated(el);
       });
 
-      it('no fetch', function() {
-        expect(this.fetchStub).to.not.have.been.called;
+      it('no fetch', () => {
+        expect(fetchStub).to.not.have.been.called;
       });
 
       testCardsRendering(null);
     });
   });
 
-  describe('Given: collection attribute is set to a truthy string', function() {
-    beforeEach(function() {
-      this.fetchStub.callsFake(mockResponse200(collectionMock));
+  describe('Given: collection attribute is set to a truthy string', () => {
+    beforeEach(() => {
+      fetchStub.callsFake(mockResponse200(collectionMock));
     });
 
-    beforeEach(async function() {
-      this.el = await fixture(`<dc-repetition collection="${this.mockCollectionName}"></dc-repetition>`);
+    beforeEach(async () => {
+      el = await fixture(`<dc-repetition collection="${mockCollectionName}"></dc-repetition>`);
     });
 
     testFetch(/\/data\/foo\.json$/);
@@ -213,7 +225,7 @@ describe('dc-repetition component', function() {
     testCardsRendering(collectionMock);
   });
 
-  afterEach(function() {
-    this.fetchStub.restore();
+  afterEach(() => {
+    fetchStub.restore();
   });
 });
